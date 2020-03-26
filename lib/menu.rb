6872562023498b3df707767c101e2488c26ba7cc
Adam Payne <contact@adam-payne.info>
2020-03-26 13:29:10 +1100

    merge

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,14 +1,42 @@
+require 'colorize'
 require 'tty-prompt'
+require 'tty-box'
+prompt=TTY::Prompt.new
 
 
+userselect=prompt.select("What would you like to do?", %w(Start About Exit))
+
+if userselect == "Start"
+    #code
+    p "start"
+elsif userselect == "About"
+    def draw_frame_about()
+         TTY::Box.frame(
+          width:40,
+          height: 10,
+          style: {bg: :yellow,},
+          align: :center,
+          padding: 2
+          ) do
+          "Pomodoro Timer designed by Adam, Andrew and Chris".colorize(:color => :black, :background=> :yellow)
+          end
+      end
+
+     puts draw_frame_about()
+elsif userselect == "Exit"
+    system = "clear"
+    puts "Bye, have a good day!"
+    sleep(2)
+    
+end
 # while true
 #   menu()
 
 #   if menu_choice == 
 #     while true
-draw frame
-refresh screen
-30s
+# draw frame
+# refresh screen
+# 30s
 #     end 
 #   elsif 
 
--- main.rb
+++ main.rb
@@ -2,24 +2,37 @@
 require 'tty-box'
 require 'tty-prompt'
 require 'tty-screen'
+require 'tty-cursor'
 require 'tty-progressbar'
 require 'io/console'
+require 'rainbow'
 
 ### Libs ###
 require_relative 'lib/frame'
 require_relative 'lib/menu'
+require_relative 'lib/art'
 
 # while true
+splash screen
 #   menu()
 
 #   if menu_choice == 
-#     while true
-      draw frame
-      refresh screen
-      30s
+#     while trues
+      main_timer
+        draw frame
+        refresh screen
+        30s
+      short_timer
+        art 
+        frame
+        refresh
+
+      long_timer
+        art
+        frame
+        refresh
 #     end 
-#   elsif 
-    
+      exit app
 #   else
     
 #   end
