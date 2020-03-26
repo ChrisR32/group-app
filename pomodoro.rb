6aa8f18ca35475cd065934d9473e9fb6408c4bb6
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-03-26 12:04:10 +1100

    logo update added tomatos

--- pomodoro.rb
+++ pomodoro.rb
@@ -7,26 +7,92 @@ $exit = "noway"
 
 system ("clear")
 
-print cursor.move_to(3, 1)
-print Rainbow("                                                                88                                   ").cadetblue  
-print cursor.move_to(3, 2)
-print Rainbow("                                                                88                                   ").cadetblue  
-print cursor.move_to(3, 3)
-print Rainbow("                                                                88                                   ").cadetblue  
-print cursor.move_to(3, 4)
-print Rainbow("8b,dPPYba,   ,adPPYba,  88,dPYba,,adPYba,   ,adPPYba, ").purple + Rainbow("  ,adPPYb,88  ,adPPYba,  8b,dPPYba,  ,adPPYba, ").cadetblue  
-print cursor.move_to(3, 5)
-print Rainbow("88P'    '8a a8'     '8a 88P'   '88'    '8a a8'     '8a").purple + Rainbow(" a8'    `Y88 a8'     '8a 88P'   'Y8 a8'     '8a").cadetblue  
-print cursor.move_to(3, 6)
-print Rainbow("88       d8 8b       d8 88      88      88 8b       d8").purple + Rainbow(" 8b       88 8b       d8 88         8b       d8").cadetblue  
-print cursor.move_to(3, 7)
-print Rainbow("88b,   ,a8' '8a,   ,a8' 88      88      88 '8a,   ,a8'").purple + Rainbow(" '8a,   ,d88 '8a,   ,a8' 88         '8a,   ,a8'").cadetblue  
-print cursor.move_to(3, 8)
-print Rainbow("88`YbbdP''   `'YbbdP''  88      88      88  `'YbbdP'' ").purple + Rainbow("  `'8bbdP'Y8  `'YbbdP''  88          `'YbbdP'' ").cadetblue  
-print cursor.move_to(3, 9)
-print Rainbow("88                                                    ").purple                                               
-print cursor.move_to(3, 10)
-print Rainbow("88").purple
+def print_logo(cursor, start_row, start_line)
+print cursor.move_to(start_row, start_line)
+print Rainbow("                                                                88                                   ").palevioletred  
+start_line += 1
+print cursor.move_to(start_row, start_line)
+print Rainbow("                                                                88                                   ").palevioletred  
+start_line += 1
+print cursor.move_to(start_row, start_line)
+print Rainbow("                                                                88                                   ").palevioletred  
+start_line += 1
+print cursor.move_to(start_row, start_line)
+print Rainbow("8b,dPPYba,   ,adPPYba,  88,dPYba,,adPYba,   ,adPPYba, ").orangered + Rainbow("  ,adPPYb,88  ,adPPYba,  8b,dPPYba,  ,adPPYba, ").palevioletred  
+start_line += 1
+print cursor.move_to(start_row, start_line)
+print Rainbow("88P'    '8a a8'     '8a 88P'   '88'    '8a a8'     '8a").orangered + Rainbow(" a8'    `Y88 a8'     '8a 88P'   'Y8 a8'     '8a").palevioletred  
+start_line += 1
+print cursor.move_to(start_row, start_line)
+print Rainbow("88       d8 8b       d8 88      88      88 8b       d8").orangered + Rainbow(" 8b       88 8b       d8 88         8b       d8").palevioletred  
+start_line += 1
+print cursor.move_to(start_row, start_line)
+print Rainbow("88b,   ,a8' '8a,   ,a8' 88      88      88 '8a,   ,a8'").orangered + Rainbow(" '8a,   ,d88 '8a,   ,a8' 88         '8a,   ,a8'").palevioletred  
+start_line += 1
+print cursor.move_to(start_row, start_line)
+print Rainbow("88`YbbdP''   `'YbbdP''  88      88      88  `'YbbdP'' ").orangered + Rainbow("  `'8bbdP'Y8  `'YbbdP''  88          `'YbbdP'' ").palevioletred  
+start_line += 1
+print cursor.move_to(start_row, start_line)
+print Rainbow("88                                                    ").orangered                                               
+start_line += 1
+print cursor.move_to(start_row, start_line)
+print Rainbow("88").orangered
+end
+
+def print_tomato(cursor, start_row, start_line)
+  
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("                   ,").green
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("                  /.\\").green
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("                 //_`\\").green
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("            _.-`| \\ ``._").green
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("        .-''").red + Rainbow("`-.       _.'").green + Rainbow("`.       ").red
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("      .'      ").red + Rainbow("/ /'\\/`.\\").green + Rainbow("    `.      ").red
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("     /   .    ").red + Rainbow("|/").green + Rainbow("         `.  \\     ").red
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("    '   /                  \\  ;    ").red
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("   :   '            \\       : :    ").red
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("   ;  ;             ;      /  .    ").red
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("    ' :             .     '  /     ").red
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("     \\ \\           /       .'      ").red
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("      `.`        .'      .'        ").red
+        start_line += 1
+        print cursor.move_to(start_row, start_line)
+        print Rainbow("        `-..___....----`           ").red
+
+ 
+    end
+
+print_tomato(cursor, 3, 12)
+
+print_tomato(cursor, 40, 12)
+
+print_tomato(cursor, 80, 12)
+
+print_logo(cursor, 4, 1)
 
 until $exit == "yes"
     $exit = gets.chomp
